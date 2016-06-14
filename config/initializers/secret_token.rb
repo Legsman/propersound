65181966ea232a1844ed3dee4ad53adc414ee316
Antoine <maciaantoine@yahoo.fr>
2016-06-14 06:07:48 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Propersound::Application.config.secret_key_base = '2336e7dffb7cd610a135b56cb0afd06fb668cca8d35a54af450603d724bac7fa319c2f2ffca3c41f02ef96dae27022685ef15ea8e86b0445c42fba6fc06ef46a'
