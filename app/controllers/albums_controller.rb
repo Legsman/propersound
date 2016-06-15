40f0702b41ca2adbb46f3753cc35cc15a7232ae4
Antoine <maciaantoine@yahoo.fr>
2016-06-15 00:38:51 +0000

    Corrected CSS, Linked /new to index

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,10 +1,22 @@
 class AlbumsController < ApplicationController
 
   def index 
-    @albums= Album.all
+    @album = Album.all
   end
 
   def new
-    
+    @album = Album.new
   end
+
+  def create
+    Album.create(album_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:name, :description, :artist)
+  end
+
 end
